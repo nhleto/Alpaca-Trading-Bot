0c21fbf2a1bd3bcb3f6c0a093f72e3ce2d0a3779
Henry <hnr.leto11@gmail.com>
2023-03-31 08:49:16 -0400

    appsettings

--- Alpaca.Console/Program.cs
+++ Alpaca.Console/Program.cs
@@ -1,10 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 
-Console.WriteLine("Hello, World!");
-
-var builder = new ConfigurationBuilder()
+var config = new ConfigurationBuilder()
+  .SetBasePath(Directory.GetCurrentDirectory())
   .AddJsonFile($"appsettings.json", true, true)
-  .AddJsonFile("appsettings.local.json", true, true);
+  .AddJsonFile("appsettings.local.json", true, true)
+  .Build();
 
-var config = builder.Build();
+var alpacaApi = config.GetSection("AlpacaApi:Endpoint");
+
